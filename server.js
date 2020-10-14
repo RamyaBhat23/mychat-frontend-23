73c338115f626c78ee55475456f4311b09f0fa27
RamyaBhat23 <ramya.shi.1111@gmail.com>
2020-10-14 19:29:22 +0530

    mychat frontend

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 const app = express();
-app.use(express.static('/dist/my-chat'));
+app.use(express.static('/src'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', { root: '/dist/my-chat' });
+    res.sendFile('index.html', { root: '/src' });
 });
 app.listen(process.env.PORT || 8080, () => {
     console.log("http://localhost:8080");
